d499582705136c2a18377c7858c8dad0c42316cf
Gilson Paulino <gilsonbp@gmail.com>
2019-08-04 11:04:03 -0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gpauth',
+    version='0.0.1',
+    description='GP Auth is an extension of the original Django Auth with some changes for modern applications.',
+    url='git@github.com:gilsonbp/gpauth.git',
+    author='Gilson Paulino',
+    author_email='gilsonbp@gmail.com',
+    license='MIT',
+    packages=['gpauth'],
+    zip_safe=False
+)
