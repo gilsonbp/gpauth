603466c515d16393a75af59079984b140bc2268c
Gilson Paulino <gilsonbp@gmail.com>
2019-11-01 16:55:38 -0300

    Update permissions and models

--- gppermissions.py
+++ gppermissions.py
@@ -1,17 +1,24 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin, AccessMixin
+from django.shortcuts import redirect
 from django.utils.translation import gettext_lazy as _
 
 
 class GPLoginRequiredMixin(AccessMixin):
-    permission_denied_message = _('You do not have permission to access this system resource.')
+    permission_denied_message = _('You do not have permission to '
+                                  'access this system resource.')
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             messages.error(request, self.get_permission_denied_message())
             return self.handle_no_permission()
 
-        return super(GPLoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+        if not hasattr(request.user, 'profile'):
+            return redirect('/admin')
+
+        return super(GPLoginRequiredMixin, self).dispatch(
+            request, *args, **kwargs
+        )
 
 
 class GPPermissions(PermissionRequiredMixin):
